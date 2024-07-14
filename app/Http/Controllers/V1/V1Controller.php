e83ebda9f5e7d7af8fa1e9fb949b1dd7d7fb9e36
stanislavlysenko <stanislav.lysenko0912@gmail.com>
2024-07-14 22:30:15 +0300

    🌐 (V1Controller.php): update test server URL in OpenAPI annotations for better accuracy

--- app/Http/Controllers/V1/V1Controller.php
+++ app/Http/Controllers/V1/V1Controller.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use OpenApi\Attributes as OA;
 
+#[OA\Server(url: "https://laravel-ta.thelysenko.dev", description: "Test Server")]
 #[OA\Server(url: "http://localhost:8000/api/v1", description: "Local Development Server")]
-#[OA\Server(url: "http://localhost:81/", description: "Test Server")]
 #[OA\SecurityScheme(
     securityScheme: 'tokenAuth',
     type: 'apiKey',
